83e47361a6e34904266f76d0333c39f29749da9d
nk-on <johndoe@example.com>
2024-12-10 22:18:52 +0400

    Adding space for displaying calculated data

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Container from './components/container/container'
 function App() {
 
   return (
     <>
-      
+      <Container />
     </>
   )
 }
