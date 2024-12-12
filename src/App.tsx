45ac240b5d61be44f1295903e4215e0aa3042e2e
nk-on <2nkoz@proton.me>
2024-12-11 21:51:03 -0800

    Deleting unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Container from './components/container/container'
 function App() {
