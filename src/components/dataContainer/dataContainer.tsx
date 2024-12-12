87957cc43e29c5f68b49fbf68b3ea6a4a47e2731
nk-on <2nkoz@proton.me>
2024-12-11 21:45:04 -0800

    Feat:adding containers which will display results

---
+++ src/components/dataContainer/dataContainer.tsx
@@ -0,0 +1,11 @@
+export default function DataContainer({title,amount}:{title:string,amount:string}){
+    return <div className="flex justify justify-around w-full">
+        <div>
+            <p className="text-[#F4FAFA] font-bold">{title}</p>
+            <p>/person</p>
+        </div>
+        <div>
+            <p className="text-[#26c0ab]">{amount}</p>
+        </div>
+    </div>
+}
