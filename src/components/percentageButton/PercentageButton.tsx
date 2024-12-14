51be6e8e9ac7ef4838e5ae565c9b65be93c7e0ce
nk-on <2nkoz@proton.me>
2024-12-13 22:55:04 -0800

    Feat:calculating and displaying total tip amount

--- src/components/percentageButton/PercentageButton.tsx
+++ src/components/percentageButton/PercentageButton.tsx
@@ -1,9 +1,10 @@
-export default function PercentageButton({percentage,setPercentage,bill,people,setTipAmount}){
+export default function PercentageButton({percentage,setPercentage,bill,people,setTipAmount, setamountPerPerson}){
     return <>
         <button className=" h-[50px] bg-[#00494d] text-[#fff] rounded-[5px]" onClick={
             ()=>{
                 setPercentage(percentage);
-                setTipAmount((bill*percentage)/100)
+                setTipAmount((bill*percentage)/100);
+                setamountPerPerson(((bill*percentage)/100)*people)
             }
         }>
             {percentage}
