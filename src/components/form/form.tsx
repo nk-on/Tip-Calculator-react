abb2e1ff8088fd1116c91e946156a3874dc1f14d
nk-on <2nkoz@proton.me>
2024-12-14 20:48:25 -0800

    Refactor:adding types to the props

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,13 +1,18 @@
-export default function Form( {title,setBill,setPeople}) {
+interface propType{
+  title:string,
+  setBill:React.Dispatch<React.SetStateAction<number>>,
+  setPeople:React.Dispatch<React.SetStateAction<number>>,
+}
+export default function Form( {title,setBill,setPeople}:propType){
   return (
     <>
       <div className="flex flex-col">
         {title}
         <input type="number" className="w-[80%] h-[50px] bg-[#c4e4e6]" onChange={(event)=>{
           if(title === "Bill"){
-            setBill(event.target.value)
+            setBill(Number(event.target.value))
           }else{
-            setPeople(event.target.value)
+            setPeople(Number(event.target.value))
           }
         }}/>
       </div>
--- src/components/percentageButton/PercentageButton.tsx
+++ src/components/percentageButton/PercentageButton.tsx
@@ -1,4 +1,12 @@
-export default function PercentageButton({percentage,setPercentage,bill,people,setTipAmount, setTotalAmount}){
+interface propType{
+    percentage:number,
+    setPercentage: React.Dispatch<React.SetStateAction<number>>,
+    bill:number,
+    people:number,
+    setTipAmount:React.Dispatch<React.SetStateAction<number>>,
+    setTotalAmount:React.Dispatch<React.SetStateAction<number>>,
+}
+export default function PercentageButton({percentage,setPercentage,bill,people,setTipAmount, setTotalAmount}:propType){
     return <>
         <button className=" h-[50px] bg-[#00494d] text-[#fff] rounded-[5px]" onClick={
             ()=>{
