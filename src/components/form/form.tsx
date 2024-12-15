06d6845c53048d875fbb3ffa0b45c033caee55a4
nk-on <2nkoz@proton.me>
2024-12-14 20:20:49 -0800

    Adding titles to the form components

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -2,7 +2,7 @@ export default function Form( {title,setBill,setPeople}) {
   return (
     <>
       <div className="flex flex-col">
-        {title.title}
+        {title}
         <input type="number" className="w-[80%] h-[50px] bg-[#c4e4e6]" onChange={(event)=>{
           if(title === "Bill"){
             setBill(event.target.value)
