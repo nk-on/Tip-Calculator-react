5003e28a6b6007d526e8c012e16ae2e95a520f87
nk-on <johndoe@example.com>
2024-12-24 21:40:36 +0400

    Style:styling reset button hover and round border

--- src/components/resetButton/resetButton.tsx
+++ src/components/resetButton/resetButton.tsx
@@ -7,7 +7,7 @@ interface propType{
     setTotalAmount:React.Dispatch<React.SetStateAction<number>>,
 }
 export default function ResetButton({currentPercentage,setError,setBill,setPeople,setTipAmount,setTotalAmount}:propType){
-    return <button className="bg-[#0C686D] w-[80%] h-[30px] uppercase" onClick={()=>{
+    return <button className="bg-[#0C686D] w-[80%] h-[30px] uppercase hover:bg-[#C4E4E6] rounded-[5px]" onClick={()=>{
         currentPercentage.current = 0;
         setError(false);
         setBill("");
